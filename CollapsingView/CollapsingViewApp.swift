615d12e5de339c2edf08681dd84bc6d337f1abd0
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2022-01-11 13:04:41 +0600

    Initial Commit

---
+++ CollapsingView/CollapsingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollapsingViewApp.swift
+//  CollapsingView
+//
+//  Created by Shakhawat Hossain Shahin on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CollapsingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
